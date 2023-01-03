419c48839ad3eb89765895d21b8ee17453236388
okmtyuta <okmtyuta@gmail.com>
2023-01-03 20:06:24 +0900

    apiの更新

--- packages/presentation/api/src/article/article.controller.ts
+++ packages/presentation/api/src/article/article.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from '@okmtyuta-engineering/infrastructure/lib/command/monopoly/okmtyuta-engineering-api/article/dto/CreateArticle.dto';
 
@@ -10,6 +10,13 @@ export class ArticleController {
   async getAllArticle() {
     const articles = this.articleService.getArticles();
     return articles;
+  }
+
+  @Get('detail/:articleId')
+  async getArticleById(@Param('articleId') articleId: string) {
+    console.log(articleId);
+    const article = this.articleService.getArticleById(articleId);
+    return article;
   }
 
   @Post('create')
--- packages/presentation/api/src/article/article.service.ts
+++ packages/presentation/api/src/article/article.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { FetchAllArticles } from '@okmtyuta-engineering/infrastructure/lib/query/monopoly/okmtyuta-engineering/article/FetchAllArticles';
+import { FetchArticleById } from '@okmtyuta-engineering/infrastructure/lib/query/monopoly/okmtyuta-engineering/article/FetchArticleById';
 import { CreateArticleDto } from '@okmtyuta-engineering/infrastructure/lib/command/monopoly/okmtyuta-engineering-api/article/dto/CreateArticle.dto';
 import { CreateArticle } from '@okmtyuta-engineering/infrastructure/lib/command/monopoly/okmtyuta-engineering-api/article/CreateArticle';
 import { DeleteAllArticle } from '@okmtyuta-engineering/infrastructure/lib/command/monopoly/okmtyuta-engineering-api/article/DeleteAllArticles';
@@ -8,9 +9,15 @@ import { AppDataSource } from 'src/db/data-source';
 @Injectable()
 export class ArticleService {
   async getArticles() {
-    const articles = (await new FetchAllArticles(AppDataSource).fetch())
-      .articles;
+    const articles = await new FetchAllArticles(AppDataSource).execute();
     return articles;
+  }
+
+  async getArticleById(articleId) {
+    const article = await new FetchArticleById(AppDataSource).execute({
+      articleId: articleId,
+    });
+    return article;
   }
 
   async createArticle(params: CreateArticleDto) {
